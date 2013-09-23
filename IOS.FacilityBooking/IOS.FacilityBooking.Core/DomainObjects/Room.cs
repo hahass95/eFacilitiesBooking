c0083e6c914fd4f143555d3d61c6e05e836e003e
clakmalk <lakmalk1981@gmail.com>
2013-09-23 08:45:24 +0530

    Upload

---
+++ IOS.FacilityBooking/IOS.FacilityBooking.Core/DomainObjects/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOS.FacilityBooking.Core.DomainObjects
+{
+    public class Room : Facility
+    {
+        public int RoomId { get; set; }
+        public decimal Area { get; set; }
+      
+    }
+}
