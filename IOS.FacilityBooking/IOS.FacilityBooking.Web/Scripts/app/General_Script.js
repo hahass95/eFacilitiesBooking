8e5eb1f6a3dba8c97044105a752837ebd10569a0
Yiming <yi_ming_1995@hotmail.com>
2013-09-27 17:38:00 +0800

    Update

--- IOS.FacilityBooking/IOS.FacilityBooking.Web/Scripts/app/General_Script.js
+++ IOS.FacilityBooking/IOS.FacilityBooking.Web/Scripts/app/General_Script.js
@@ -93,18 +93,18 @@ $("#registerForm").submit(registerFormSubmitHandler);
 });
 
 // Calendar
-$(function updateCalendar() {
+$(function () {
 
 	var date = new Date();
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
 
-	var calendar = $('#calendar').myCalendar({
+	var calendar = $('#calendar').fullCalendar({
 		header: {
 			left: 'agendaWeek,agendaDay',
 			center: 'title',
-			right: 'prev,next today'
+			right: 'today prev,next'
 		},
 		height: "550",
 		theme: true,
@@ -126,54 +126,8 @@ $(function updateCalendar() {
 			}
 			calendar.fullCalendar('unselect');
 		},
-		editable: false,
-		events: [
-			{
-				title: 'All Day Event',
-				start: new Date(y, m, 1)
-			},
-			{
-				title: 'Long Event',
-				start: new Date(y, m, d - 5),
-				end: new Date(y, m, d - 2)
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: new Date(y, m, d - 3, 16, 0),
-				allDay: false
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: new Date(y, m, d + 4, 16, 0),
-				allDay: false
-			},
-			{
-				title: 'Meeting',
-				start: new Date(y, m, d, 10, 30),
-				allDay: false
-			},
-			{
-				title: 'Lunch',
-				start: new Date(y, m, d, 12, 0),
-				end: new Date(y, m, d, 14, 0),
-				allDay: false
-			},
-			{
-				title: 'Birthday Party',
-				start: new Date(y, m, d + 1, 19, 0),
-				end: new Date(y, m, d + 1, 22, 30),
-				allDay: false
-			},
-			{
-				title: 'Click for Google',
-				start: new Date(y, m, 28),
-				end: new Date(y, m, 29),
-				url: 'http://google.com/'
-			}
-		]
-	});
+		editable: false
+		});
 });
 
 //var clock;
