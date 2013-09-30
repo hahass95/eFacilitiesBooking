4d9b141d0364a2190dce65e976716183a2d56aa8
Yiming <yi_ming_1995@hotmail.com>
2013-09-30 18:01:00 +0800

    Update Again

---
+++ IOS.FacilityBooking/IOS.FacilityBooking.Web/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOS.FacilityBooking.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+		public ActionResult Home(string returnUrl)
+        {
+			ViewBag.ReturnUrl = returnUrl;
+			return View();
+        }
+
+    }
+}
